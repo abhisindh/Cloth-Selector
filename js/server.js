1dca16f613f3178f58ae564e924809a957f5ffcf
Abhisindh Chatterjee <abhisindh123@gmail.com>
2021-08-04 23:01:24 +0530

    admin page

--- js/server.js
+++ js/server.js
@@ -1,11 +1,9 @@
 
 function reset(){
     if (confirm('Are you sure you want to reset the order of list items?')){
-    var setOne={'IMG_20210709_200308.jpg': '.', 'IMG_20210709_200357.jpg': '.', 'IMG_20210709_200340.jpg': '.', 'IMG_20210709_200408.jpg': '.', 'IMG_20210709_200419.jpg': '.', 'IMG_20210709_200432.jpg': '.', 'IMG_20210709_200445.jpg': '.', 'IMG_20210709_200454.jpg': '.', 'IMG_20210709_200501.jpg': '.', 'IMG_20210709_200512.jpg': '.', 'IMG_20210709_200521.jpg': '.', 'IMG_20210709_200536.jpg': '.', 'IMG_20210709_200545.jpg': '.', 'IMG_20210709_200552.jpg': '.', 'IMG_20210709_200600.jpg': '.', 'IMG_20210709_200648.jpg': '.', 'IMG_20210709_200832.jpg': '.', 'IMG_20210709_200913.jpg': '.', 'IMG_20210712_092905.jpg': '.'}   
-    var setTwo={'IMG_20210804_150133.jpg': '.', 'IMG_20210804_150151.jpg': '.', 'IMG_20210804_150215.jpg': '.', 'IMG_20210804_150226.jpg': '.', 'IMG_20210804_150457.jpg': '.', 'IMG_20210804_150509.jpg': '.', 'IMG_20210804_150519.jpg': '.'}
-    
-    localStorage.setItem('setOne',JSON.stringify(setOne));
-    localStorage.setItem('setTwo',JSON.stringify(setTwo));
+        
+    localStorage.setItem('setOne',JSON.stringify(setOneInit));
+    localStorage.setItem('setTwo',JSON.stringify(setTwoInit));
     }
     
 }
