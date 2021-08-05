65a6b6a4aa5b7c2efed3796ecaef16a1a9ba231f
Abhisindh Chatterjee <abhisindh123@gmail.com>
2021-08-05 14:11:57 +0530

    collection object

---
+++ js/collection.js
@@ -0,0 +1,32 @@
+var collectionObj={'setOne':setOneInit,'setTwo':setTwoInit,'setThree':setThreeInit,'setFour':setFourInit,'setFive':setFiveInit,'setSix':setSixInit,'setSeven':setSevenInit,'setEight':setEightInit,'setNine':setNineInit,'setTen':setTenInit,'setEleven':setElevenInit,'setTwelve':setTwelveInit}
+        
+
+
+var date=Date.now()
+var setOneInit={'IMG_20210804_150133.jpg': date, 'IMG_20210804_150151.jpg': date, 'IMG_20210804_150215.jpg': date, 'IMG_20210804_150226.jpg': date, 'IMG_20210804_150457.jpg': date, 'IMG_20210804_150509.jpg': date, 'IMG_20210804_150519.jpg': date}
+var setTwoInit={'IMG_20210709_200308.jpg': date, 'IMG_20210709_200357.jpg': date, 'IMG_20210709_200340.jpg': date, 'IMG_20210709_200408.jpg': date, 'IMG_20210709_200419.jpg': date, 'IMG_20210709_200432.jpg': date, 'IMG_20210709_200445.jpg': date, 'IMG_20210709_200454.jpg': date, 'IMG_20210709_200501.jpg': date, 'IMG_20210709_200512.jpg': date, 'IMG_20210709_200521.jpg': date, 'IMG_20210709_200536.jpg': date, 'IMG_20210709_200545.jpg': date, 'IMG_20210709_200552.jpg': date, 'IMG_20210709_200600.jpg': date, 'IMG_20210709_200648.jpg': date, 'IMG_20210709_200832.jpg': date, 'IMG_20210709_200913.jpg': date, 'IMG_20210712_092905.jpg': date}   
+var setThreeInit={'IMG_20210804_150151.jpg': date, 'IMG_20210804_150215.jpg': date, 'IMG_20210804_150226.jpg': date, 'IMG_20210804_150457.jpg': date, 'IMG_20210804_150509.jpg': date, 'IMG_20210804_150519.jpg': date}
+var setFourInit={'IMG_20210804_150151.jpg': date, 'IMG_20210804_150215.jpg': date, 'IMG_20210804_150226.jpg': date, 'IMG_20210804_150457.jpg': date, 'IMG_20210804_150509.jpg': date, 'IMG_20210804_150519.jpg': date}
+var setFiveInit={'IMG_20210804_150151.jpg': date, 'IMG_20210804_150215.jpg': date, 'IMG_20210804_150226.jpg': date, 'IMG_20210804_150457.jpg': date, 'IMG_20210804_150509.jpg': date, 'IMG_20210804_150519.jpg': date}
+var setSixInit={'IMG_20210804_150151.jpg': date, 'IMG_20210804_150215.jpg': date, 'IMG_20210804_150226.jpg': date, 'IMG_20210804_150457.jpg': date, 'IMG_20210804_150509.jpg': date, 'IMG_20210804_150519.jpg': date}
+var setSevenInit={'IMG_20210804_150151.jpg': date, 'IMG_20210804_150215.jpg': date, 'IMG_20210804_150226.jpg': date, 'IMG_20210804_150457.jpg': date, 'IMG_20210804_150509.jpg': date, 'IMG_20210804_150519.jpg': date}
+var setEightInit={'IMG_20210804_150151.jpg': date, 'IMG_20210804_150215.jpg': date, 'IMG_20210804_150226.jpg': date, 'IMG_20210804_150457.jpg': date, 'IMG_20210804_150509.jpg': date, 'IMG_20210804_150519.jpg': date}
+var setNineInit={'IMG_20210804_150151.jpg': date, 'IMG_20210804_150215.jpg': date, 'IMG_20210804_150226.jpg': date, 'IMG_20210804_150457.jpg': date, 'IMG_20210804_150509.jpg': date, 'IMG_20210804_150519.jpg': date}
+var setTenInit={'IMG_20210804_150151.jpg': date, 'IMG_20210804_150215.jpg': date, 'IMG_20210804_150226.jpg': date, 'IMG_20210804_150457.jpg': date, 'IMG_20210804_150509.jpg': date, 'IMG_20210804_150519.jpg': date}
+var setElevenInit={'IMG_20210804_150151.jpg': date, 'IMG_20210804_150215.jpg': date, 'IMG_20210804_150226.jpg': date, 'IMG_20210804_150457.jpg': date, 'IMG_20210804_150509.jpg': date, 'IMG_20210804_150519.jpg': date}
+var setTwelveInit={'IMG_20210804_150151.jpg': date, 'IMG_20210804_150215.jpg': date, 'IMG_20210804_150226.jpg': date, 'IMG_20210804_150457.jpg': date, 'IMG_20210804_150509.jpg': date, 'IMG_20210804_150519.jpg': date}
+
+
+
+var backOne=localStorage.getItem('backOne')
+var backTwo=localStorage.getItem('backTwo')
+var backThree=localStorage.getItem('backThree')
+var backFour=localStorage.getItem('backFour')
+var backFive=localStorage.getItem('backFive')
+var backSix=localStorage.getItem('backSix')
+var backSeven=localStorage.getItem('backSeven')
+var backEight=localStorage.getItem('backEight')
+var backNine=localStorage.getItem('backNIne')
+var backTen=localStorage.getItem('backTen')
+var backEleven=localStorage.getItem('backEleven')
+var backTwelve=localStorage.getItem('backTwelve')
