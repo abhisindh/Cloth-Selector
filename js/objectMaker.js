ba11a5ada69fff64cde2585a86fb6a3663995403
Abhisindh Chatterjee <abhisindh123@gmail.com>
2021-08-15 19:03:39 +0530

    object maker translated to js

---
+++ js/objectMaker.js
@@ -0,0 +1,13 @@
+text = `IMG_20210815_181340.jpg
+IMG_20210815_181520.jpg
+IMG_20210815_181601.jpg
+IMG_20210815_181638.jpg`
+
+// TEST
+obj={}
+JSON.stringify(text)
+arr=text.split("\n")
+for (i=0;i<arr.length;i++){
+    obj[arr[i]]=Date.now()
+}
+console.log(obj);
