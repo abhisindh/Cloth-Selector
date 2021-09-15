1f917f4b917ae5228a4b07c7937b5b03b63cd38e
Abhisindh Chatterjee <abhisindh123@gmail.com>
2021-09-15 21:57:07 +0530

    rename complete

--- js/objectMaker.js
+++ js/objectMaker.js
@@ -1,17 +1,25 @@
-text = `IMG_20210818_091647.jpg
-IMG_20210818_091711.jpg
-IMG_20210818_091724.jpg
-IMG_20210818_091746.jpg
-IMG_20210818_091815.jpg
-IMG_20210818_100349.jpg
-IMG_20210818_100733.jpg
-IMG_20210818_100751.jpg`
+text=`SC_001.jpg
+SC_002.jpg
+SC_003.jpg
+SC_004.jpg
+SC_005.jpg
+SC_006.jpg
+SC_007.jpg
+SC_008.jpg
+SC_009.jpg
+SC_010.jpg
+SC_011.jpg
+SC_012.jpg
+SC_013.jpg
+SC_014.jpg
+SC_015.jpg
+SC_016.jpg`
 
 // TEST
 obj={}
 JSON.stringify(text)
 arr=text.split("\n")
 for (i=0;i<arr.length;i++){
-    obj[arr[i]]=Date.now()
+    obj[arr[i]]=Date.now()-(86400000*(arr.length-i-1))
 }
 console.log(JSON.stringify(obj).replace(/'/g, '"'));
