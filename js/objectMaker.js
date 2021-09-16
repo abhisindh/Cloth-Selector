46feaa140d9b24e2547124d00ecae12f74949bdb
Abhisindh Chatterjee <abhisindh123@gmail.com>
2021-09-16 10:09:13 +0530

    placeholder

--- js/objectMaker.js
+++ js/objectMaker.js
@@ -1,19 +1,9 @@
-text=`SC_001.jpg
-SC_002.jpg
-SC_003.jpg
-SC_004.jpg
-SC_005.jpg
-SC_006.jpg
-SC_007.jpg
-SC_008.jpg
-SC_009.jpg
-SC_010.jpg
-SC_011.jpg
-SC_012.jpg
-SC_013.jpg
-SC_014.jpg
-SC_015.jpg
-SC_016.jpg`
+text=`Z_1.png
+Z_2.png
+Z_3.png
+Z_4.png
+Z_5.png
+Z_6.png`
 
 // TEST
 obj={}
